237af6da73b13b037301515241a46ca868a318e5
Beni Stocker <benjamin.stocker@gmail.com>
2024-05-01 11:26:14 +0200

    added script

---
+++ analysis/make_tidy_cmip6.R
@@ -0,0 +1,24 @@
+library(map2tidy)
+library(dplyr)
+library(ggplot2)
+
+# list demo file path
+path <- "/data/scratch/CMIP6ng/cmip6-ng/pr/day/native/"
+
+## Precipitation ---------------
+files <- list.files(path, pattern = "pr_day", full.names = TRUE)
+
+# load and convert
+df <- map2tidy(
+  nclist = files,
+  varnam = "pr",
+  lonnam = "lon",
+  latnam = "lat",
+  timenam = "time",
+  timedimnam = "time",
+  do_chunks = TRUE,
+  outdir = "/data/scratch/CMIP6ng/cmip6_tidy/",
+  fileprefix = "pr_day_CESM2_historical_r1i1p1f1_native"
+)
+
+
