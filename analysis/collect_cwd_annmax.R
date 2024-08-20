45c147949b1ff6074e2c3c4c4644a18f4bdf4d9b
Fabian Bernhard <10245680+fabern@users.noreply.github.com>
2024-08-20 14:43:48 +0200

    Update collect_cwd_annmax

--- analysis/collect_cwd_annmax.R
+++ analysis/collect_cwd_annmax.R
@@ -1,85 +1,59 @@
 #!/usr/bin/env Rscript
 
-# script is called with three arguments:
-# 1. counter for chunks
-# 2. total number of chunks
-# 3. total number of longitude indices
+# script is called without any arguments
 
 # Example:
-# >./get_cwd_annmax.R 1 3 360
-
-# to receive arguments to script from the shell
-args = commandArgs(trailingOnly=TRUE)
-
-# # When using this script directly from RStudio, not from the shell, specify
-# nlon <- 288  # set this by hand. corresponds to length of the longitude dimension in original NetCDF files
-# args <- c(1, 1, nlon)
+# >./collect_cwd_annmax.R
 
 library(dplyr)
 library(map2tidy)
 library(multidplyr)
 
-source(paste0(here::here(), "/R/collect_cwd_annmax_byilon.R"))
+indir  <- "/data_2/scratch/fbernhard/cmip6-ng/tidy/cwd/"
 
-print("getting data for longitude indices:")
-vec_index <- map2tidy::get_index_by_chunk(
-  as.integer(args[1]),  # counter for chunks
-  as.integer(args[2]),  # total number of chunks
-  as.integer(args[3])   # total number of longitude indices
-  )
-
-# number of cores of parallel threads
-ncores <- 2 # parallel::detectCores()
-
-# parallelize job
-# set up the cluster, sending required objects to each core
-cl <- multidplyr::new_cluster(ncores) |>
-  multidplyr::cluster_library(c("map2tidy",
-                                "dplyr",
-                                "purrr",
-                                "tidyr",
-                                "readr",
-                                "here",
-                                "magrittr")) |>
-  multidplyr::cluster_assign(
-    collect_cwd_annmax_byilon = collect_cwd_annmax_byilon   # make the function known for each core
-    )
-
-# distribute computation across the cores, calculating for all longitudinal
-# indices of this chunk
-df <- tibble(ilon =  vec_index) |>
-  multidplyr::partition(cl) |>
-  dplyr::mutate(out = purrr::map(
-    ilon,
-    ~collect_cwd_annmax_byilon(
-      .,
-      indir = "~/data/cmip6-ng/tidy/cwd/",
-      fileprefix = "evspsbl_cum"
-      ))
-    ) |>
-  collect() |>
-  tidyr::unnest(out)
-
-readr::write_rds(
-  df,
-  paste0(
-    indir,
-    fileprefix,
-    "_ANNMAX.rds"
-  )
+# 1) Define filenames of files to process:  -------------------------------
+filnams <- list.files(
+  indir,
+  pattern = "evspsbl_cum_LON_[0-9.+-]*_ANNMAX.rds", # make sure to include only _ANNMAX.rds
+  full.names = TRUE
 )
 
+    # NOTE: this script has to be run non-parallel, since it collects results
+    #       from previous scripts that were run in parallel.
+    # # 2) Setup parallelization ------------------------------------------------
+    # # 2a) Split job onto multiple nodes
+    # #     i.e. only consider a subset of the files (others might be treated by another compute node)
+    # vec_index <- map2tidy::get_index_by_chunk(
+    #   as.integer(args[1]),  # counter for compute node
+    #   as.integer(args[2]),  # total number of compute node
+    #   length(filnams)       # total number of longitude indices
+    # )
+    #
+    # # 2b) Parallelize job across cores on a single node
+    # ncores <- 2 # parallel::detectCores() # number of cores of parallel threads
+    #
+    # cl <- multidplyr::new_cluster(ncores) |>
+    #   # set up the cluster by sending required objects to each core
+    #   multidplyr::cluster_library(c("map2tidy",
+    #                                 "dplyr",
+    #                                 "purrr",
+    #                                 "tidyr",
+    #                                 "readr",
+    #                                 "here",
+    #                                 "magrittr")) |>
+    #   multidplyr::cluster_assign(
+    #     collect_cwd_annmax_byilon = collect_cwd_annmax_byilon  # make the function known for each core
+    #   )
 
-# # un-parallel alternative
-# df <- tibble(ilon = seq(60)) |>     # vec_index
-#   dplyr::mutate(out = purrr::map(
-#     ilon,
-#     ~collect_cwd_annmax_byilon(
-#       .,
-#       indir = "~/data/cmip6-ng/tidy/cwd/",
-#       fileprefix = "evspsbl_cum"
-#     ))
-#   ) |>
-#   tidyr::unnest(out)
-#
-# format(object.size(df), units = "MB")
+
+# 3) Process files --------------------------------------------------------
+global_df <- lapply(filnams,
+              function(filnam) {readr::read_rds(filnam) |> tidyr::unnest(data)}) |>
+  bind_rows()
+
+readr::write_rds(global_df,
+                 file.path(indir,paste0(fileprefix, "_ANNMAX.rds"))
+
+
+
+
