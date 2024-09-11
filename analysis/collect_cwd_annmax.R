57fb6e3c41626ea84bd8395d47c1e81a9f14031a
Fabian Bernhard <10245680+fabern@users.noreply.github.com>
2024-09-11 12:44:28 +0200

    feat: Add output of cwd in addition to pcwd

--- analysis/collect_cwd_annmax.R
+++ analysis/collect_cwd_annmax.R
@@ -9,40 +9,54 @@ library(dplyr)
 library(map2tidy)
 library(multidplyr)
 
-indir       <- "/data_1/CMIP6/tidy/pcwd_reset/test/" 
-# indir       <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/" 
-# outfile_act <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/act_evspsbl_cum_ANNMAX.nc" # adjust path to where the file should be written to
-outfile_pot <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/pot_evspsbl_cum_ANNMAX" # adjust path to where the file should be written to
-# TODO: generate subfolder of outfile_pot and outfile_act
+indir_pcwd     <- "/data_1/CMIP6/tidy/pcwd_reset/test/"
+indir_cwd      <- "/data_1/CMIP6/tidy/cwd_reset/test/"
+# indir       <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/"
+outfile_cwd  <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/act_evspsbl_cum_ANNMAX.nc" # adjust path to where the file should be written to
+outfile_pcwd <- "/data_2/scratch/fbernhard/CMIP6/tidy/pcwd_reset/test/pot_evspsbl_cum_ANNMAX" # adjust path to where the file should be written to
 
 # 1) Define filenames of files to collect:  -------------------------------
-# filnams_pcwd <- list.files(indir, pattern = "pcwd_[0-9]*.rds",        full.names = TRUE)
-# filnams_pcwd <- list.files(indir, pattern = "pcwd_[0-9]*_ANNMAX.rds",        full.names = TRUE)
 filnams_pcwd <- list.files(
-  indir, 
-  pattern = "pcwd_[0-9]*_ANNMAX.rds", # make sure to include only _ANNMAX.rds
-  # pattern = "CWD_result_LON_[0-9.+-]*_ANNMAX.rds", # make sure to include only _ANNMAX.rds
+  indir_pcwd,
+  pattern = "^pcwd_[0-9]*_ANNMAX.rds$", # make sure to include only _ANNMAX.rds
+  # pattern = "CWD_result_LON_[0-9.+-]*_ANNMAX.rds", # make sure to include only _ANNMAX.rds # TODO: change filename with map2tidy update
+  full.names = TRUE
+)
+filnams_cwd <- list.files(
+  indir_cwd,
+  pattern = "^cwd_[0-9]*_ANNMAX.rds$", # make sure to include only _ANNMAX.rds
+  # pattern = "CWD_result_LON_[0-9.+-]*_ANNMAX.rds", # make sure to include only _ANNMAX.rds # TODO: change filename with map2tidy update
   full.names = TRUE
 )
 
 if (length(filnams_pcwd) <= 1){
   stop("Should find multiple files. Only found " ,length(filnams_pcwd), ".")
 }
+if (length(filnams_cwd) <= 1){
+  stop("Should find multiple files. Only found " ,length(filnams_cwd), ".")
+}
 
 # 3) Process files --------------------------------------------------------
 df_pcwd <- lapply(filnams_pcwd,
               function(filnam) {readr::read_rds(filnam) |> tidyr::unnest(data)}) |>
   bind_rows()
+df_cwd <- lapply(filnams_cwd,
+                  function(filnam) {readr::read_rds(filnam) |> tidyr::unnest(data)}) |>
+  bind_rows()
 
+# TODO: generate subfolder of outfile_pcwd and outfile_cwd
+dir.create(dirname(outfile_pcwd), showWarnings = FALSE, recursive = TRUE)
 readr::write_rds(
   df_pcwd,
-  paste0(outfile_pot, ".rds")) 
-  # file.path(indir,paste0(fileprefix, "_ANNMAX.rds"))
+  paste0(outfile_pcwd, ".rds"), compress = "xz") # file.path(indir,paste0(fileprefix, "_ANNMAX.rds"))
+readr::write_rds(
+  df_cwd,
+  paste0(outfile_cwd, ".rds"), compress = "xz") # file.path(indir,paste0(fileprefix, "_ANNMAX.rds"))
 
-# Instead of writing rds file, directly save as NetCDF:
 
 
 # 4) Output to single, global NetCDF file ---------------------------------
+# TODO(fabian): add another netCDF output for actual evspsbl
 library(rgeco)  # get it from https://github.com/geco-bern/rgeco
 
 # create object that can be used with write_nc2()
@@ -91,15 +105,14 @@ rgeco::write_nc2(
   obj,
   varnams = "pot_evspsbl_cum",
   make_tdim = TRUE,
-  path = paste0(outfile_pot, ".nc"),
+  path = paste0(outfile_pcwd, ".nc"),
   units_time = "days since 2001-01-01",
   att_title      = "Global Potential Cumulative Water Deficit",
   att_history    = sprintf(
     "Created on: %s, with R scripts from (%s) processing input data from: %s",
-    Sys.Date(), gitrepo_id, indir)
+    Sys.Date(), gitrepo_id, indir_pcwd)
 )
 
-# TODO(fabian): add another netCDF output for actual evspsbl
 
 
 
