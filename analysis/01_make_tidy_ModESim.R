4e221fcf05a27cc52bfc5bf69827accb4eb3cee5
Helpap <phelpap@giub.local@climcal3.giub.unibe.ch>
2024-09-17 16:43:37 +0200

    make_tidy for ModE-Sim, precip only so far

---
+++ analysis/01_make_tidy_ModESim.R
@@ -0,0 +1,114 @@
+library(map2tidy)
+library(dplyr)
+library(stringr)
+
+# list demo file path
+# adjust path to where your ModE-Sim data is located
+path_ModESim <- "~/scratch2/precip"
+
+outdir <- "~/scratch2/tidy"
+
+## Precipitation - daily resolution--------------------------------------------------
+path_ModESim <- "~/scratch2"
+varnam <- "precip";
+filnam <- list.files(
+  paste0(path_ModESim, "/", varnam),
+  pattern = ".nc", full.names = TRUE)
+output_dir = file.path(outdir, "1420_01_m001_precip")
+
+# Process each file individually
+# for (i in seq_along(filnam)) {
+  file <- filnam[i]
+  prefix <- str_remove(basename(filnam[1]), "_1420.nc")
+  prefix <- "set1420_1_m001_precip"
+# convert to tidy
+res_pr <- map2tidy(
+  nclist = filnam[1:2],
+  varnam = "precip",
+  lonnam = "lon",
+  latnam = "lat",
+  timenam = "time",
+  do_chunks = TRUE,
+  outdir = output_dir,
+  fileprefix = "set1420_1_m001_precip",
+  ncores = 1,
+  overwrite = FALSE
+  )
+# }
+# Check if any unsuccessful:
+stopifnot(nrow(res_pr |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+
+
+## Temperature - monthly resolution-------------------------------------------------
+varnam <- "tas"; res <- "day"
+filnam <- list.files(
+  paste0(path_cmip6, varnam, "/", res, "/native/"),
+  pattern = ".nc", full.names = TRUE)
+if (length(filnam) != 1){stop("Should find only a single file.")}
+
+# convert to tidy
+res_tas <- map2tidy(
+  nclist = filnam,
+  varnam = "tas",
+  lonnam = "lon",
+  latnam = "lat",
+  timenam = "time",
+  do_chunks = TRUE,
+  outdir = file.path(outdir, "01_tas"),
+  fileprefix = str_remove(basename(filnam), ".nc"),
+  ncores = 20,   # parallel::detectCores()
+  overwrite = FALSE
+)
+# Check if any unsuccessful:
+stopifnot(nrow(res_tas |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+
+
+## Net Radiation - monthly resolution -------------------------------------------
+
+varnam <- "rlus"; res <- "mon"
+filnam <- list.files(
+  paste0(path_cmip6, varnam, "/", res, "/native/"),
+  pattern = ".nc", full.names = TRUE)
+if (length(filnam) != 1){stop("Should find only a single file.")}
+
+# convert to tidy
+res_rlus <- map2tidy(
+  nclist = filnam,
+  varnam = "rlus",
+  lonnam = "lon",
+  latnam = "lat",
+  timenam = "time",
+  do_chunks = TRUE,
+  outdir = file.path(outdir, "01_rlus"),
+  fileprefix = str_remove(basename(filnam), ".nc"),
+  ncores = 20,   # parallel::detectCores()
+  overwrite = FALSE
+)
+# Check if any unsuccessful:
+stopifnot(nrow(res_rlus |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+
+
+
+## Surface Pressure - monthly resolution ----------------------------------------
+varnam <- "rsus"; res <- "mon"
+filnam <- list.files(
+  paste0(path_cmip6, varnam, "/", res, "/native/"),
+  pattern = ".nc", full.names = TRUE)
+if (length(filnam) != 1){stop("Should find only a single file.")}
+
+# convert to tidy
+res_rsus <- map2tidy(
+  nclist = filnam,
+  varnam = "rsus",
+  lonnam = "lon",
+  latnam = "lat",
+  timenam = "time",
+  do_chunks = TRUE,
+  outdir = file.path(outdir, "01_rsus"),
+  fileprefix = str_remove(basename(filnam), ".nc"),
+  ncores = 20,   # parallel::detectCores()
+  overwrite = FALSE
+)
+# Check if any unsuccessful:
+stopifnot(nrow(res_rsus |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+
