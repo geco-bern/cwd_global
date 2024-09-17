9e8c1f89f39ed6cfbda5a984985c6efe532a6bd0
Helpap <phelpap@giub.local@climcal3.giub.unibe.ch>
2024-09-17 16:44:27 +0200

    tidy precip for US-Ton station

--- analysis/01_make_tidy_ModESim.R
+++ analysis/01_make_tidy_ModESim.R
@@ -16,14 +16,11 @@ filnam <- list.files(
   pattern = ".nc", full.names = TRUE)
 output_dir = file.path(outdir, "1420_01_m001_precip")
 
-# Process each file individually
-# for (i in seq_along(filnam)) {
-  file <- filnam[i]
-  prefix <- str_remove(basename(filnam[1]), "_1420.nc")
+
   prefix <- "set1420_1_m001_precip"
 # convert to tidy
 res_pr <- map2tidy(
-  nclist = filnam[1:2],
+  nclist = filnam[1:431],
   varnam = "precip",
   lonnam = "lon",
   latnam = "lat",
@@ -32,14 +29,40 @@ res_pr <- map2tidy(
   outdir = output_dir,
   fileprefix = "set1420_1_m001_precip",
   ncores = 1,
-  overwrite = FALSE
+  overwrite = FALSE,
+  filter_lon_between_degrees = c(-122, -120) #longitude of US-Ton: -120.9660
   )
-# }
+
+# test <- readRDS("~/scratch2/tidy/1420_01_m001_precip/set1420_1_m001_precip_LON_-120.000.rds")
+#
+# test %>% slice(1) %>% unnest(data) %>%
+#   tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
+#   mutate(datetime = lubridate::ymd(datetime))
+
+# Get a list of RDS files in the directory
+file_list <- list.files(paste0(outdir, "/", "1420_01_m001_precip"), pattern = "\\.rds$", full.names = TRUE)
+
+# Loop through each file
+for (file in file_list) {
+  # Read the RDS file
+  test <- readRDS(file)
+
+  # Modify the datetime format
+  test_modified <- test %>%
+    slice(1) %>%
+    unnest(data) %>%
+    tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
+    mutate(datetime = lubridate::ymd(datetime))
+
+  # Save the modified data back to the RDS file, overwriting the original
+  saveRDS(test_modified, file)
+}
+
 # Check if any unsuccessful:
 stopifnot(nrow(res_pr |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
 
 
-## Temperature - monthly resolution-------------------------------------------------
+## Temperature - daily resolution-------------------------------------------------
 varnam <- "tas"; res <- "day"
 filnam <- list.files(
   paste0(path_cmip6, varnam, "/", res, "/native/"),
