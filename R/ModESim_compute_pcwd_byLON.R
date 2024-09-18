41635c6167eb852f70cb95d5c6a88fc2e0e23dc3
Helpap <phelpap@giub.local@climcal3.giub.unibe.ch>
2024-09-18 16:54:44 +0200

    made changes to fit ModESim data

---
+++ R/ModESim_compute_pcwd_byLON.R
@@ -0,0 +1,160 @@
+ModESim_compute_pcwd_byLON <- function(
+    LON_string,
+    indir,
+    outdir){
+
+  #############################################
+  # Define hardcoded paths and hardcoded options:
+  indir_patm      <- file.path(indir, "1420_01_m001_patm")
+  indir_tas       <- file.path(indir, "1420_01_m001_tsurf")
+  indir_prec      <- file.path(indir, "1420_01_m001_precip")
+  indir_netrad    <- file.path(indir, "1420_01_m001_netrad")
+
+  # # indir_elevation <- file.path(indir, "01_elevation") --------not needed as have Psurf
+  # indir_elevation <- file.path("/data_1/CMIP6/tidy/", "elevation")
+
+  path_pcwd <- file.path(outdir, paste0("ModESim_pcwd", "_", LON_string, ".rds"))
+
+  # prepare output names
+  # stopifnot(grepl(pattern = "\\[LONSTRING\\]", outfilename_template))
+  # outpath <- file.path(outdir, gsub("\\[LONSTRING\\]", LON_string, outfilename_template))
+  #############################################
+
+
+  print(paste0(Sys.time(), ", LON: ", LON_string))
+
+  # load functions that will be applied to time series
+  source(paste0(here::here(), "/R/get_cwd_withSnow_and_reset.R"))
+
+  # read from files that contain tidy data for a single longitudinal band
+  # read surface Pressure file tidy
+  filnam <- file.path(indir_patm, paste0("set1420_1_m001_patm_",
+                                            LON_string,".rds"))
+  df_patm <- readr::read_rds(filnam)
+
+
+  # read precipitation file tidy
+  filnam <- file.path(indir_prec, paste0("set1420_1_m001_precip_",
+                                         LON_string,".rds"))
+  df_prec <- readr::read_rds(filnam)
+
+
+  # read temperature file tidy
+  filnam <- file.path(indir_tas, paste0("set1420_1_m001_tsurf_",
+                                         LON_string,".rds"))
+  df_tas <- readr::read_rds(filnam)
+
+
+  # read net radiation file tidy
+  filnam <- file.path(indir_netrad, paste0("set1420_1_m001_netrad_",
+                                        LON_string,".rds"))
+  df_net_radiation <- readr::read_rds(filnam)
+
+
+  # # read elevation file and convert to data frame
+  # # library(terra)
+  # filnam <- file.path(indir_elevation, "elevation.nc")
+  # rasta_elevation <- terra::rast(filnam)
+
+  ## read the needed longitude value and extract the latitude values
+  lon <- df_prec[["lon"]][1]
+  latitudes <- unique(df_prec$lat)
+
+  ## create a data frame of the coordinates
+  loc <- data.frame(lon = rep(lon, length(latitudes)), lat = latitudes)
+
+  ## convert to vector points
+  points <- terra::vect(loc, geom = c("lon", "lat"), crs = "EPSG:4326")
+
+  # ## extract values
+  # vals <- terra::extract(rasta_elevation, points, xy = FALSE, ID = FALSE, method = "simple")
+  #
+  # # combine coordinates with extracted values
+  # df_elevation <- data.frame(
+  #   lon = lon,
+  #   lat = latitudes,
+  #   value = vals
+  # )
+  #
+  # ## set NA-value at 90 degrees to 0
+  # df_elevation[is.na(df_elevation)] <- 0
+
+
+  # unnest all the data frames
+  df_net_radiation  <- net_radiation |> tidyr::unnest(data)
+  df_patm           <- df_patm   |> tidyr::unnest(data)
+  df_prec           <- df_prec |> tidyr::unnest(data) # lon lat pr time
+  df_tas            <- df_tas  |> tidyr::unnest(data)
+
+
+  # unit conversions
+  ## precipitation; total precip has units of kg m-2 s-1
+  df_prec <-  df_prec |>
+    mutate(pr = pr * 86400 ) # conversion to mm day-1
+
+  ## temperature
+  df_tas <-  df_tas |>
+    mutate(tas = tas - 273.15) # conversion to °C
+
+  # data wrangling and time resolution adjustments
+
+  ## extract year and month from the time column---------------????????????
+  df_prec <- df_prec |>
+    mutate(time = lubridate::ymd_hms(datetime)) |>
+    mutate(year = lubridate::year(time), month = lubridate::month(time)) |>
+    select(-datetime)
+
+  df_tas <- df_tas |>
+    mutate(time = lubridate::ymd_hms(datetime)) |>
+    mutate(year = lubridate::year(time), month = lubridate::month(time)) |>
+    select(-datetime)
+
+  df_patm <- df_patm |>
+    mutate(time = lubridate::ymd_hms(datetime)) |>
+    mutate(year = lubridate::year(time), month = lubridate::month(time)) |>
+    select(-time, -datetime)
+
+  df_net_radiation <- df_net_radiation |>
+    mutate(time = lubridate::ymd_hms(datetime)) |>
+    mutate(year = lubridate::year(time), month = lubridate::month(time)) |>
+    select(-time, -datetime)
+################################################????????????????????????????
+
+  ### merge all such that monthly data is repeated for each day within month
+  ## pcwd
+  df_pcwd <- df_prec |>  # one of the daily data frames
+    left_join(df_net_radiation, by = join_by(lon, lat, year, month)) |>
+    left_join(df_patm, by = join_by(lon, lat, year, month)) |>
+    left_join(df_tas, by = join_by(lon, lat, time)) |>
+    dplyr::select(-year, -month)
+
+  # pet-calculation
+  # ## calculate surface pressure
+  # source(paste0(here::here(), "/R/calc_patm.R"))
+  # df_pcwd$patm <- calc_patm(df_pcwd$elevation)
+  ## apply pet() function
+  df_pcwd <- df_pcwd |>
+    mutate(pet = 60 * 60 * 24 * cwd::pet(net_radiation, tas, patm)) # conversion from mm s-1 to mm day-1
+
+
+  # out pcwd
+  out_pcwd <- df_pcwd |>
+    select(lon, lat, time, pr, tas, evspsbl = pet) |> # Use POTENTIAL ET as ET estimate
+
+    # group data by grid cells and wrap time series for each grid cell into a new
+    # column, by default called 'data'.
+    group_by(lon, lat) |>
+    tidyr::nest() |> dplyr::ungroup() |>
+
+    # apply the custom function on the time series data frame separately for
+    # each grid cell.
+    ###slice(1:2)|> # uncomment for development/debugging
+    mutate(data = purrr::map(data, ~get_cwd_withSnow_and_reset(.), .progress = TRUE))
+
+  # write (complemented) data to cwd- and pcwd-files with meaningful name and index counter
+  message(paste0("Writing file ", path_pcwd, " ..."))
+  readr::write_rds(out_pcwd, path_pcwd)
+
+  # don't return data - it's written to file
+  return(NULL)
+}
--- analysis/01_make_tidy_ModESim.R
+++ analysis/01_make_tidy_ModESim.R
@@ -41,25 +41,43 @@ res_pr <- map2tidy(
 # Get a list of RDS files in the directory
 file_list <- list.files(paste0(outdir, "/", "1420_01_m001_precip"), pattern = "\\.rds$", full.names = TRUE)
 
-# Loop through each file
-for (file in file_list) {
-  # Read the RDS file
-  test <- readRDS(file)
 
-  # Modify the datetime format
-  test_modified <- test %>%
-    slice(1) %>%
-    unnest(data) %>%
-    tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
-    mutate(datetime = lubridate::ymd(datetime))
+# # Loop through each file
+# for (file in file_list) {
+#   # Read the RDS file
+#   test <- readRDS(file)
+#
+#   # Modify the datetime format
+#   test_modified <- test %>%
+#     slice(1) %>%
+#     unnest(data) %>%
+#     tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
+#     mutate(datetime = lubridate::ymd(datetime))
+#
+#   # Save the modified data back to the RDS file, overwriting the original
+#   saveRDS(test_modified, file)
+# }
 
-  # Save the modified data back to the RDS file, overwriting the original
-  saveRDS(test_modified, file)
-}
+# list.files(tmpdir)
+df <- readRDS(file.path(outdir, "/", "1420_01_m001_precip", "set1420_1_m001_precip_LON_-121.875.rds"))
 
-# Check if any unsuccessful:
-stopifnot(nrow(res_pr |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+# # test snippet to develop the separate and mutate command
+# df |> dplyr::slice(1) |> tidyr::unnest(data) |>
+#   tidyr::separate(datetime, sep = "\\.", into = c('date', 'fract_day')) |>
+#   dplyr::mutate(date = lubridate::ymd(date))
 
+# apply it to all nested data.frames using purrr::map
+df2 <- df |> dplyr::mutate(data = purrr::map(data, function(x){
+  x |>
+    tidyr::separate(datetime, sep = "\\.", into = c('date', 'fract_day')) |>
+    dplyr::mutate(date = lubridate::ymd(date)) |> dplyr::select(-fract_day)
+}))
+
+saveRDS(df2, file.path(outdir, "/", "1420_01_m001_precip", "set1420_1_m001_precip_LON_-121.875.rds"))
+
+
+# # check result for the first element:
+# df2 |> dplyr::slice(1) |> tidyr::unnest(data)
 
 ## Temperature - daily resolution-------------------------------------------------
 path_ModESim <- "~/scratch2"
@@ -93,27 +111,47 @@ res_ts <- map2tidy(
 #   mutate(datetime = lubridate::ymd(datetime))
 
 # Get a list of RDS files in the directory
-file_list <- list.files(paste0(outdir, "/", "1420_01_m001_tsurf"), pattern = "\\.rds$", full.names = TRUE)
+#file_list <- list.files(paste0(outdir, "/", "1420_01_m001_tsurf"), pattern = "\\.rds$", full.names = TRUE)
 
-# Loop through each file
-for (file in file_list) {
-  # Read the RDS file
-  test <- readRDS(file)
+# # Loop through each file
+# for (file in file_list) {
+#   # Read the RDS file
+#   test <- readRDS(file)
+#
+#   # Modify the datetime format
+#   test_modified <- test %>%
+#     slice(1) %>%
+#     unnest(data) %>%
+#     tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
+#     mutate(datetime = lubridate::ymd(datetime))
+#
+#   # Save the modified data back to the RDS file, overwriting the original
+#   saveRDS(test_modified, file)
+# }
+#
+# test1 <- readRDS("~/scratch2/tidy/1420_01_m001_tsurf/set1420_1_m001_tsurf_LON_-121.875.rds")
+# # Check if any unsuccessful:
+# stopifnot(nrow(res_ts |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+#test1$data
 
-  # Modify the datetime format
-  test_modified <- test %>%
-    slice(1) %>%
-    unnest(data) %>%
-    tidyr::separate(datetime, into = c("datetime", "fractional_day"), sep = "\\.") %>%
-    mutate(datetime = lubridate::ymd(datetime))
+# list.files(tmpdir)
+df <- readRDS(file.path(outdir, "/", "1420_01_m001_tsurf", "set1420_1_m001_tsurf_LON_-120.000.rds"))
 
-  # Save the modified data back to the RDS file, overwriting the original
-  saveRDS(test_modified, file)
-}
+# # test snippet to develop the separate and mutate command
+# df |> dplyr::slice(1) |> tidyr::unnest(data) |>
+#   tidyr::separate(datetime, sep = "\\.", into = c('date', 'fract_day')) |>
+#   dplyr::mutate(date = lubridate::ymd(date))
 
-test1 <- readRDS("~/scratch2/tidy/1420_01_m001_precip/set1420_1_m001_precip_LON_-120.000.rds")
-# Check if any unsuccessful:
-stopifnot(nrow(res_ts |> tidyr::unnest(data) |> filter(!grepl("Written",data))) == 0)
+# apply it to all nested data.frames using purrr::map
+df2 <- df |> dplyr::mutate(data = purrr::map(data, function(x){
+  x |>
+    tidyr::separate(datetime, sep = "\\.", into = c('date', 'fract_day')) |>
+    dplyr::mutate(date = lubridate::ymd(date)) |> dplyr::select(-fract_day)
+}))
+
+saveRDS(df2, file.path(outdir, "/", "1420_01_m001_tsurf", "set1420_1_m001_tsurf_LON_-120.000.rds"))
+
+
 
 
 ## Net Radiation - monthly resolution -------------------------------------------
@@ -123,26 +161,53 @@ filnam <- list.files(
   paste0(path_ModESim, "/", varnam),
   pattern = ".nc", full.names = TRUE)
 output_dir = file.path(outdir, "1420_01_m001_netrad")
-
-
 prefix <- "set1420_1_m001_netrad"
-# convert to tidy
+
+# for files that do not contain a date variable:
+# define a function that derives the necessary dates from the filename and pass that function to fgetdate
+# see ?map2tidy
+# see example: https://github.com/geco-bern/grsofun/blob/084cbdc3c1c5094f3b43e0869f898abdcb973ff8/R/grsofun_tidy.R#L89-L96
+library(dplyr)
+library(purrr)
+filename_to_monthly_datelist <- function(filename){
+  # e.g. filename <- "~/Downloads/set1420_1_m001_precip_1420.nc"
+  # year <- as.numeric(gsub(".*_([0-9]*).nc","\\1", basename(filename)))
+  year_str <- gsub(".*_([0-9]+)_mon.netrad.nc","\\1", basename(filename))
+  return(sprintf("%s-%02d", year_str, c(1:12)))
+}
+
+# # Create a list of date lists using lapply
+# date_lists <- lapply(filnam[1:2], filename_to_monthly_datelist)
+
 res_nr <- map2tidy(
   nclist = filnam[1:431],
   varnam = "netrad",
   lonnam = "longitude",
   latnam = "latitude",
   timenam = "Time",
-  do_chunks = TRUE,
-  outdir = output_dir,
-  fileprefix = "set1420_1_m001_netrad",
-  ncores = 1,
+  do_chunks = TRUE, ncores = 1, fgetdate = filename_to_monthly_datelist,
+  outdir = output_dir, fileprefix = "set1420_1_m001_netrad",
   overwrite = FALSE,
   filter_lon_between_degrees = c(-122, -120) #longitude of US-Ton: -120.9660
 )
 
-test1 <- readRDS("~/scratch2/tidy/1420_01_m001_netrad/set1420_1_m001_netrad_LON_-120.000.rds")
-test1$data
+# # convert to tidy
+# res_nr <- map2tidy(
+#   nclist = filnam[1:431],
+#   varnam = "netrad",
+#   lonnam = "longitude",
+#   latnam = "latitude",
+#   timenam = "Time",
+#   do_chunks = TRUE,
+#   outdir = output_dir,
+#   fileprefix = "set1420_1_m001_netrad",
+#   ncores = 1,
+#   overwrite = FALSE,
+#   filter_lon_between_degrees = c(-122, -120) #longitude of US-Ton: -120.9660
+# )
+#
+ test1 <- readRDS("~/scratch2/tidy/1420_01_m001_netrad/set1420_1_m001_netrad_LON_-120.000.rds")
+# test1$data
 
 ## Surface Pressure - monthly resolution ----------------------------------------
 path_ModESim <- "~/scratch2"
@@ -153,6 +218,19 @@ filnam <- list.files(
 output_dir = file.path(outdir, "1420_01_m001_patm")
 
 
+# for files that do not contain a date variable:
+# define a function that derives the necessary dates from the filename and pass that function to fgetdate
+# see ?map2tidy
+# see example: https://github.com/geco-bern/grsofun/blob/084cbdc3c1c5094f3b43e0869f898abdcb973ff8/R/grsofun_tidy.R#L89-L96
+library(dplyr)
+library(purrr)
+filename_to_monthly_datelist <- function(filename){
+  # e.g. filename <- "~/Downloads/set1420_1_m001_precip_1420.nc"
+  # year <- as.numeric(gsub(".*_([0-9]*).nc","\\1", basename(filename)))
+  year_str <- gsub(".*_([0-9]+)_mon.patm.nc","\\1", basename(filename))
+  return(sprintf("%s-%02d", year_str, c(1:12)))
+}
+
 prefix <- "set1420_1_m001_patm"
 # convert to tidy
 res_patm <- map2tidy(
@@ -161,6 +239,7 @@ res_patm <- map2tidy(
   lonnam = "longitude",
   latnam = "latitude",
   timenam = "Time",
+  fgetdate = filename_to_monthly_datelist,
   do_chunks = TRUE,
   outdir = output_dir,
   fileprefix = "set1420_1_m001_patm",
@@ -169,5 +248,5 @@ res_patm <- map2tidy(
   filter_lon_between_degrees = c(-122, -120) #longitude of US-Ton: -120.9660
 )
 
-test1 <- readRDS("~/scratch2/tidy/1420_01_m001_patm/set1420_1_m001_patm_LON_-120.000.rds")
-test1$data
+# test1 <- readRDS("~/scratch2/tidy/1420_01_m001_patm/set1420_1_m001_patm_LON_-120.000.rds")
+# test1$data
