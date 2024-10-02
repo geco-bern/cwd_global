9d379da846ff38407c2b5efd7f7c11c4eb5b4452
PHelpap <145592069+PHelpap@users.noreply.github.com>
2024-10-02 09:58:50 +0200

    Update apply_cwd_global.sh

--- src/apply_cwd_global.sh
+++ src/apply_cwd_global.sh
@@ -3,9 +3,9 @@
 #SBATCH --time=504:00:00
 #SBATCH --partition=icpu-stocker # if you have access, this gives you priority
 #SBATCH --ntasks=1               # nr of tasks (processes), used for MPI jobs that may run distributed on multiple compute nodes
-#SBATCH --cpus-per-task=124      # nr of threads, used for shared memory jobs that run locally on a single compute node (default: 1)
-# ###SBATCH --mail-user=patricia.helpap@students.unibe.ch
-# ###SBATCH --mail-type=fail               # when do you want to get notified: none, all, begin, end, fail, requeue, array_tasks
+#SBATCH --cpus-per-task=128      # nr of threads, used for shared memory jobs that run locally on a single compute node (default: 1)
+#SBATCH --mail-user=patricia.helpap@students.unibe.ch
+#SBATCH --mail-type=fail               # when do you want to get notified: none, all, begin, end, fail, requeue, array_tasks
 #SBATCH --chdir=cwd_global/analysis  # define here the working directory which contains your R-script, and where the output will be written to; no tilde ~/ necessary
 
 echo "Started on: $(date --rfc-3339=seconds)"
