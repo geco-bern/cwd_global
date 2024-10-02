df4f45d2f8cb1af708229cf96e70cecf69bda407
PHelpap <145592069+PHelpap@users.noreply.github.com>
2024-10-02 11:53:08 +0200

    Update apply_cwd_global.sh

--- src/apply_cwd_global.sh
+++ src/apply_cwd_global.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name="PCWD calculation 1420 set"
 #SBATCH --time=16-00:00:00
 # ###SBATCH --partition=icpu-stocker # if you have access, this gives you priority
-#SBATCH --partition=job_cpu_long
+# ###SBATCH --partition=job_cpu_long
 #SBATCH --ntasks=1               # nr of tasks (processes), used for MPI jobs that may run distributed on multiple compute nodes
 #SBATCH --cpus-per-task=50      # nr of threads, used for shared memory jobs that run locally on a single compute node (default: 1)
 #SBATCH --mail-user=patricia.helpap@students.unibe.ch
