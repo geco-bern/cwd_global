0ea3a1844c49bc9c6d3a152af21ce3a65b3f0e6f
fabern <10245680+fabern@users.noreply.github.com>
2026-03-25 12:47:36 +0100

    Update SLURM batch scripts memory requests

--- src/ERA5Land-fullResNoNA/main-noNA_02pcwd.sh
+++ src/ERA5Land-fullResNoNA/main-noNA_02pcwd.sh
@@ -6,10 +6,10 @@
 #SBATCH --ntasks=1             # nr of tasks (processes), used for MPI jobs that may run distributed on multiple compute nodes
 #SBATCH --array=1-200%12       # specifies the slurm array job with the number of tasks (at most 12 running simultaneously to restrict hogging of our nodes (12*160GB = 1900GB with 2x1500GB leaving 1100GB free))
 #SBATCH --cpus-per-task=4      # nr of threads, used for shared memory jobs that run locally on a single compute node (default: 1)
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=30G           # First jobs showed it takes about 77GB, we thus request NCPU x 30GB = 120GB
 #SBATCH --mail-user=fabian.bernhard@unibe.ch
 #SBATCH --mail-type=fail,begin,end            # when do you want to get notified: none, all, begin, end, fail, requeue, array_tasks
-#SBATCH --dependency=afterok:48255100
+#SBATCH --dependency=afterok:8977
 #SBATCH --chdir=../cwd_global/analysis/ERA5Land-fullResNoNA  # define here the working directory which contains your R-script, and where the output will be written to; no tilde ~/ necessary
 
 echo "Started on: $(date --rfc-3339=seconds)"
--- src/ERA5Land-fullResNoNA/main-noNA_03netcdf.sh
+++ src/ERA5Land-fullResNoNA/main-noNA_03netcdf.sh
@@ -1,16 +1,15 @@
 #! /usr/bin/bash -l
-#SBATCH --job-name="ERA5Land_pcwd_create_NetCDF"
+#SBATCH --job-name="ERA5Land_pcwd_create_NetCDF_1950-1999"
 #SBATCH --time=3-00:00:00
 #SBATCH --account=invest
 #SBATCH --qos=job_icpu-stocker
 #SBATCH --ntasks=1               # nr of tasks (processes), used for MPI jobs that may run distributed on multiple compute nodes
-#SBATCH --array=2000-2024%12     # specifies the slurm array job with the number of tasks
+#SBATCH --array=1990-1999%4      # specifies the slurm array job with the number of tasks
 #SBATCH --cpus-per-task=8        # nr of threads, used for shared memory jobs that run locally on a single compute node (default: 1)
-#SBATCH --mem-per-cpu=40G
+#SBATCH --mem-per-cpu=15G           # First jobs showed it takes about 66GB, we thus request NCPU x 15GB = 120GB
 #SBATCH --mail-user=fabian.bernhard@unibe.ch
 #SBATCH --mail-type=fail            # when do you want to get notified: none, all, begin, end, fail, requeue, array_tasks
 #SBATCH --mail-type=fail,begin,end            # when do you want to get notified: none, all, begin, end, fail, requeue, array_tasks
-#SBATCH --dependency=afterok:48255167
 #SBATCH --chdir=../cwd_global/analysis/ERA5Land-fullResNoNA  # define here the working directory which contains your R-script, and where the output will be written to; no tilde ~/ necessary
 
 echo "Started on: $(date --rfc-3339=seconds)"
